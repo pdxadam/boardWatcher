f384ec7232d017ec5adf60c5dec0e007a2d9e3cc
pdxadam <pdxadam@gmail.com>
2020-09-13 15:35:44 -0700

    Initial commit

---
+++ boardPaq.py
@@ -0,0 +1,34 @@
+import requests
+import bs4
+import shelve
+import ezgmail
+
+#Get the old boardPaq future meetings list
+filename = "oldBoardMeetings"
+siteAddress = "https://www.boardpaq.com/cast?c=hG3yQ4gP3kN6yH7uJ5sB&t=N"
+try:
+    p = shelve.open(filename)
+    old = p['oldBoardMeetings']
+    p.close()
+except KeyError:
+    old = ""
+
+#Get the current boardPaq future meetings list
+res = requests.get("https://www.boardpaq.com/cast?c=hG3yQ4gP3kN6yH7uJ5sB&t=N")
+res.raise_for_status()
+boardSoup = bs4.BeautifulSoup(res.text, 'html.parser')
+futureMeetings = str(boardSoup.select('table#mdtbl')[0])
+if (futureMeetings == old):
+    print("It's all the same")
+else:
+    print(futureMeetings)
+    p = shelve.open(filename)
+    p['oldBoardMeetings'] = futureMeetings
+    p.close()
+    #This is where I want to send the email out.
+
+
+
+    ezgmail.send("pdxadam@gmail.com", "Changes to upcoming board meetings", siteAddress + "\n" + futureMeetings)
+
+
